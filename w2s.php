66094b55cea9aa51ed83105a4d53dd87f577dcd0
Stanislav Malyshev <smalyshev@gmail.com>
2015-05-19 14:15:45 -0700

    Add run URL

--- w2s.php
+++ w2s.php
@@ -58,8 +58,8 @@ Please enter WDQ query:<br>
 <?= @$_POST['wdq']; ?>
 </textarea><br>
 Syntax: <select name="syntax">
-<option label="WDTK Syntax">WDTK</option>
 <option label="Wikidata RDF syntax">Wikidata</option>
+<option label="WDTK Syntax">WDTK</option>
 </select><br>
 <input type="submit" value="Translate"/>
 <br clear="all">
@@ -69,8 +69,10 @@ Translation to SPARQL:<br>
 <pre>
 <?= htmlentities($text); ?>
 </pre>
-<?php if($run) { ?>
-<a target="_blank" href=" http://milenio.dcc.uchile.cl/sparql?query=<?= urlencode($text); ?>">Run this query!</a>
+<?php if($run) { 
+	$runURLs = array('Wikidata' => 'http://wdqs-beta.wmflabs.org/#', "WDTK" => 'http://milenio.dcc.uchile.cl/sparql?query=');
+?>
+<a target="_blank" href=" <?=$runURLs[$_POST['syntax']].urlencode($text); ?>">Run this query!</a>
 <?php } ?>
 <a href="https://github.com/smalyshev/wdq2sparql"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
 </body>
